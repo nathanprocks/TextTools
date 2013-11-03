aa23631776577b1e72e8e8050a0105a289249c86
mad monkey <M8R-479xwi@mailinator.com>
2013-11-03 08:09:06 +0000

    Move replaceSelected() into contentscript.js
    Decreases loading times (HTTP Headers etc)

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,3 +1,10 @@
+function replaceSelection(replaceText) {
+	var input = $(document.activeElement);
+	if ((input.is('input') && input.attr('type') == 'text') || input.is('textarea')) {
+		//input.val(input.val().substring(0, input.selectionStart) + replaceText + input.val().substring(input.selectionEnd));
+		input.selection('replace', {text: replaceText});
+	}
+}
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		switch (request.textTool) {
