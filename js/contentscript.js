1c9a0bd1a62a7fbae2ff16c73e77311c7c998797
mad monkey <M8R-479xwi@mailinator.com>
2013-11-03 09:17:46 +0000

    WIP Scratch forum post dropdown
    When line 9 is uncommented, a new button should appear on the editor for
    scratch forum posts. I'll add more later.

--- js/contentscript.js
+++ js/contentscript.js
@@ -5,6 +5,10 @@ function replaceSelection(replaceText) {
 		input.selection('replace', {text: replaceText});
 	}
 }
+if(window.location.href.substring(0, 30) == "http://scratch.mit.edu/discuss") {
+//$(".markItUpContainer .markItUpHeader ul").append('<li class="markItUpSeparator">---------------</li><li class="markItUpButton markItUpDropMenu markItUpTextTools"><a href="#" title="TextTools - WIP">TextTools - WIP</a><ul><li class="markItUpButton"><a href="#" title="Rainbow">Rainbow - BBCode</a></li><li class="markItUpButton"><a href="#" title="Flipped">Flipped</a></li><li class="markItUpButton"><a href="#" title="Reversed">Reversed</a></li></ul></li>');
+//Uncomment above to add button to scratch forum post editor. WIP - will add more later
+}
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		switch (request.textTool) {
