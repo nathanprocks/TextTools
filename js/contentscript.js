801c0dfaadd1f3dc886213d5736c5103180f1c61
comp500 <M8R-479xwi@mailinator.com>
2013-11-03 14:01:49 +0000

    add icon to menu thing

--- js/contentscript.js
+++ js/contentscript.js
@@ -6,7 +6,8 @@ function replaceSelection(replaceText) {
 	}
 }
 if(window.location.href.substring(0, 30) == "http://scratch.mit.edu/discuss") {
-//$(".markItUpContainer .markItUpHeader ul").append('<li class="markItUpSeparator">---------------</li><li class="markItUpButton markItUpDropMenu markItUpTextTools"><a href="#" title="TextTools - WIP">TextTools - WIP</a><ul><li class="markItUpButton"><a href="#" title="Rainbow">Rainbow - BBCode</a></li><li class="markItUpButton"><a href="#" title="Flipped">Flipped</a></li><li class="markItUpButton"><a href="#" title="Reversed">Reversed</a></li></ul></li>');
+$(".markItUpContainer .markItUpHeader > ul").append('<li class="markItUpSeparator">---------------</li><li class="markItUpButton markItUpDropMenu markItUpTextTools"><a href="#" title="TextTools - WIP">TextTools - WIP</a><ul><li class="markItUpButton"><a href="#" title="Rainbow">Rainbow - BBCode</a></li><li class="markItUpButton"><a href="#" title="Flipped">Flipped</a></li><li class="markItUpButton"><a href="#" title="Reversed">Reversed</a></li></ul></li>');
+$(".markItUpTextTools a").css("background-image", "url(" + chrome.extension.getURL("icon_16.png") + ")");
 //Uncomment above to add button to scratch forum post editor. WIP - will add more later
 }
 chrome.runtime.onMessage.addListener(
